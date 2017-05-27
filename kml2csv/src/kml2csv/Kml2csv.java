8b8719631abfb29f6ce9cc22055544d8cb891655
jcfalcon <jcfalcon@DESKTOP-U7PG468>
2017-05-27 17:42:50 +0100

    Definición de las clases

---
+++ kml2csv/src/kml2csv/Kml2csv.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kml2csv;
+
+/**
+ *
+ * @author jcfalcon
+ */
+public class Kml2csv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
